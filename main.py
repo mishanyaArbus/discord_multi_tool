2e556435f2add4b0ae0e7ff450ead5efee769023
mishanya <85110229+mishanyaArbus@users.noreply.github.com>
2022-06-01 18:33:45 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -36,7 +36,6 @@ print(
     "-----------------------",
     "1 - add reactions",
     "2 - get last messages in chat",
-    "3 - send messages to chat",
     "-----------------------",
     sep="\n")
 task = int(input("Choose task --> "))
@@ -84,24 +83,6 @@ def parse_last_msgs(ses: r.Session, chat_id, num):
         for msg in msgs[:num]:
             f.write(msg["content"].replace("\n","")+"\n")
 
-def send_messages(ses: r.Session, chat_id, message):
-    _data = {'content': message, 'tts': False}
-
-    try:
-        resp = ses.post(f'https://discord.com/api/v9/channels/{chat_id}/messages', json=_data, timeout=2)
-    except r.exceptions.ReadTimeout:
-        sleep(10)
-        send_messages(ses, chat_id, message)
-        return 
-
-    if resp.status_code == 200:
-        logger.success(f"Send message '{message}' with {ses.headers['authorization']}")
-    elif resp.status_code == 429:
-        sleep(int(resp.json()['retry_after']))
-        send_messages(ses, chat_id, message)
-    else:
-        logger.error(f"{resp.status_code} {resp.text} Failed to send message '{message}' with {ses.headers['authorization']}")
-
 def performer(task_num, auth_tok, proxy, **transit_datas):
     ses = r.Session()
     ses.headers["authorization"] = auth_tok
@@ -113,8 +94,6 @@ def performer(task_num, auth_tok, proxy, **transit_datas):
 
     if task_num == 1:
         add_reations(ses=ses, **transit_datas)
-    elif task_num == 3:
-        send_messages(ses=ses, **transit_datas)
 
 def threader():
     while True:
@@ -124,18 +103,14 @@ def threader():
 
 if __name__ == "__main__":
 
-    if not task in (2, 4):
-        for i in range(threads):
-            Thread(target=threader, daemon=True).start()
-
     if task == 1:
         c_id = input("Chat id --> ")
 
-        answ = int(input("1 - file with msg ids \n2 - auto react to last 100 messages \n-->"))
+        answ = int(input("1 - file with msg ids \n2 - auto detect \n-->"))
 
         if answ == 1:
             message_ids_l = [a.replace("\n", "") for a in open(input("File with message ids --> "), "r").readlines()]
-        else:
+        elif answ == 2:
             ses_l = r.Session()
             ses_l.headers["authorization"] = tokens[0]
 
@@ -148,17 +123,11 @@ if __name__ == "__main__":
 
         reactions_l = [a.replace("\n", "") for a in open(input("File with reactions --> "), "r", encoding = "utf8").readlines()]
 
-        task_data = {
+        task_datas = {
             "chat_id":c_id,
             "message_ids":message_ids_l,
             "reactions":reactions_l,
         }
-
-        for token in tokens:
-            q.put({"task_num":task, "auth_tok":token, "proxy":proxies.pop(0), **task_data})
-
-        q.join()
-
     elif task == 2:
         c_id = input("Chat id --> ")
         num = int(input("Number of messages --> "))
@@ -170,37 +139,13 @@ if __name__ == "__main__":
 
         parse_last_msgs(ses_l, c_id, num)
 
-    elif task == 3:
-        c_id = input("Chat id --> ")
-        messages_to_send = [a.replace("\n", "") for a in open(input("File with messages to send --> "), "r", encoding="utf8").readlines()]
-        send_type = input("1 - send messages once\n2 - send messages endlessly\n --> ")
+    if not task in (2, 3):
+        for i in range(threads):
+            Thread(target=threader, daemon=True).start()
 
-        if send_type == 2:
-            print("To stop the program close the window")
+        for token in tokens:
+            q.put({"task_num":task, "auth_tok":token, "proxy":proxies.pop(0), **task_datas})
 
-        temp_tokens = tokens.copy()
-        temp_proxy = proxies.copy()
-
-        while True:
-            for message_to_send in messages_to_send:
-
-                if len(temp_tokens) == 0 or len(temp_proxy) == 0:
-                    temp_tokens = tokens.copy()
-                    temp_proxy = proxies.copy()
-
-                task_data = {
-                    "chat_id":c_id,
-                    "message":message_to_send
-                }
-                try:
-                    q.put({"task_num": task, "auth_tok": temp_tokens.pop(0), "proxy": temp_proxy.pop(0), **task_data})
-                except Exception as e:
-                    print(temp_tokens, temp_proxy)
-                    raise e
-
-                q.join()
-            if send_type==1:
-                logger.info("-SENT FULL BATCH-")
-                break
+        q.join()
 
     input("done")
